1460485745ce1bc6c3b90ad4e058ce12a85f887f
JulienMonchany <j.monchany@adsoftware.fr>
2021-12-04 16:46:29 +0100

    chore: refacto part 1

--- src/3_BinaryDiagnostic/diagnostic.ts
+++ src/3_BinaryDiagnostic/diagnostic.ts
@@ -1,32 +1,58 @@
+import { off } from "process";
+
 type Bit = "0" | "1";
 
+type BitCounter = {
+  [b in Bit]: [];
+};
+
+const INITIAL_BIT_POSITION: BitCounter = { "0": [], "1": [] };
+
 export const getPowerConsumption = (diagnostic: string[] = []): number => {
-  if (diagnostic.length === 0) return 0;
+  const { gamma, epsilon } = calculateGammaAndEpsilon(
+    getBinaryPositions(diagnostic)
+  );
+  return gamma * epsilon;
+};
+
+const calculateGammaAndEpsilon = (
+  binaryPositions: BitCounter[]
+): { gamma: number; epsilon: number } => {
+  const { gamma, epsilon } = binaryPositions.reduce(
+    (acc, { 0: zeroes, 1: ones }) =>
+      zeroes.length > ones.length
+        ? { gamma: acc.gamma + "0", epsilon: acc.epsilon + "1" }
+        : { gamma: acc.gamma + "1", epsilon: acc.epsilon + "0" },
+    {
+      gamma: "",
+      epsilon: "",
+    }
+  );
+
+  return { gamma: parseInt(gamma, 2), epsilon: parseInt(epsilon, 2) };
+};
+
+const getBinaryPositions = (diagnostic: string[] = []): BitCounter[] => {
+  if (diagnostic.length === 0) {
+    return [INITIAL_BIT_POSITION];
+  }
+
   let bitPosition = 0;
-  let gamma = "",
-    epsilon = "";
+  const bitPositionCounter: BitCounter[] = [];
+
   while (bitPosition < diagnostic[0].length) {
-    let bitCounter = { "1": 0, "0": 0 };
+    let bitCounter = INITIAL_BIT_POSITION;
     for (const bin of diagnostic) {
       const bit = bin.charAt(bitPosition);
       if (!isBit(bit)) {
         throw new Error(`unexpected bit encountered: "${bit}"`);
       }
-      bitCounter[bit]++;
+      bitCounter = { ...bitCounter, [bit]: [...bitCounter[bit], bin] };
     }
-    if (bitCounter["0"] > bitCounter["1"]) {
-      gamma += "0";
-      epsilon += "1";
-    } else {
-      gamma += "1";
-      epsilon += "0";
-    }
+    bitPositionCounter.push(bitCounter);
     bitPosition++;
   }
-  return getPowerFromGammaAndEpsilon(gamma, epsilon);
+  return bitPositionCounter;
 };
 
 const isBit = (s: string): s is Bit => s === "0" || s === "1";
-
-const getPowerFromGammaAndEpsilon = (gamma: string, epsilon: string) =>
-  parseInt(gamma, 2) * parseInt(epsilon, 2);
